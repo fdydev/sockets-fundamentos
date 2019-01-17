1cbca8f594ab648befe6242d17e6e83c0f92a859
Fredy <fdymbx@gmail.com>
2019-01-17 18:09:20 +0100

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+// Escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('Respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
